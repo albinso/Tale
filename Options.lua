606837764ef68789165969272a1e2749999201e5
Mizukichan <mizukichan@arcor.de>
2010-11-19 19:20:15 +0100

    Initial commit

---
+++ Options.lua
@@ -0,0 +1,30 @@
+-- ********************************************************
+-- **                  Memoria - Options                 **
+-- **           <http://nanaki.affenfelsen.de>           **
+-- ********************************************************
+--
+-- This addon is written and copyrighted by:
+--    * Mizukichan @ EU-Thrall (2010)
+--
+--
+--    This file is part of Mizus RaidTracker.
+--
+--    Memoria is free software: you can redistribute it and/or 
+--    modify it under the terms of the GNU General Public License as 
+--    published by the Free Software Foundation, either version 3 of the 
+--    License, or (at your option) any later version.
+--
+--    Memoria is distributed in the hope that it will be useful,
+--    but WITHOUT ANY WARRANTY; without even the implied warranty of
+--    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--    GNU General Public License for more details.
+--
+--    You should have received a copy of the GNU General Public License
+--    along with Memoria.  
+--    If not, see <http://www.gnu.org/licenses/>.
+--
+
+-- Check for addon table
+if (not Memoria) then Memoria = {}; end
+
+local Memoria = Memoria;
---
+++ locals/localization.lua
@@ -0,0 +1,17 @@
+-- ********************************************************
+-- **             Memoria - enGB/enUS Local              **
+-- **           <http://nanaki.affenfelsen.de>           **
+-- ********************************************************
+--
+-- This localization is written by:
+--  Mizukichan
+--
+
+-- Check for addon table
+if (not Memoria) then Memoria = {}; end
+
+local Memoria = Memoria;
+
+-- Localization
+Memoria.L = {
+}
