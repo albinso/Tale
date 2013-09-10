9c4afbcb31eb60ad07b7bde2f29d45f0fa068618
Mizukichan <mizukichan@arcor.de>
2013-09-10 02:39:56 +0200

    Added localization files for itIT and ptBR.

---
+++ locals/localization_itIT.lua
@@ -0,0 +1,15 @@
+-- ********************************************************
+-- **               Memoria - itIT Local                 **
+-- **           <http://nanaki.affenfelsen.de>           **
+-- ********************************************************
+--
+-- This localization is written by:
+--  Deepking
+--
+
+-- Check for addon table
+if (not Memoria) then Memoria = {}; end
+if GetLocale() ~= "itIT" then return end
+
+-- Localization
+--@localization(locale="itIT", format="lua_additive_table", handle-unlocalized="comment", table-name="Memoria.L")@
---
+++ locals/localization_ptBR.lua
@@ -0,0 +1,15 @@
+-- ********************************************************
+-- **               Memoria - ptBR Local                 **
+-- **           <http://nanaki.affenfelsen.de>           **
+-- ********************************************************
+--
+-- This localization is written by:
+--  Deepking
+--
+
+-- Check for addon table
+if (not Memoria) then Memoria = {}; end
+if GetLocale() ~= "ptBR" then return end
+
+-- Localization
+--@localization(locale="ptBR", format="lua_additive_table", handle-unlocalized="comment", table-name="Memoria.L")@
