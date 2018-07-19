7862c4fc09f5bcab9badcb2f9c97f99856b6cc90
Mizukichan <mizukichan@arcor.de>
2018-07-20 00:17:43 +0200

    TOC bump, updates for patch 8.0

--- Memoria.lua
+++ Memoria.lua
@@ -4,7 +4,7 @@
 -- ********************************************************
 --
 -- This addon is written and copyrighted by:
---    * Mîzukichan @ EU-Antonidas (2010-2017)
+--    * Mîzukichan @ EU-Antonidas (2010-2018)
 --
 --
 --    This file is part of Memoria.
