c8d6436409c749204f8b5a79811196a739de451b
Mizukichan <mizukichan@arcor.de>
2019-06-21 00:43:11 +0200

    TOC bump

--- Memoria.lua
+++ Memoria.lua
@@ -4,7 +4,7 @@
 -- ********************************************************
 --
 -- This addon is written and copyrighted by:
---    * Mîzukichan @ EU-Antonidas (2010-2018)
+--    * Mîzukichan @ EU-Antonidas (2010-2019)
 --
 --
 --    This file is part of Memoria.
